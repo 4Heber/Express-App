44a8b3f1de8202b3c3e0d2f36adddc94692d3644
Heber <parzival.html@gmail.com>
2023-12-06 18:25:02 +0100

    asyc code example with axios module.

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,18 @@
 const { Router } = require('express');
 
 const router = Router();
+const axios = require('axios');
+
+const URL = 'https://jsonplaceholder.typicode.com/comments';
+
+router.get('/comments', async (req, res)=>{
+
+    const response = await axios.get(URL);
+
+    res.render('comments', {
+        comments: response.data
+    });
+});
 
 router.all('/UserName', (req, res)=>{
     res.send('Username route');
